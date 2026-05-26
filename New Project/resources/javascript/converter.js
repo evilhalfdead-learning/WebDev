d6988114291ff991ac4073e77574ac68f4a838aa
evilhalfdead-learning <evilhalfdead@gmail.com>
2026-05-26 09:20:00 -0400

    added feet, yards, meters

--- New Project/resources/javascript/converter.js
+++ New Project/resources/javascript/converter.js
@@ -26,20 +26,20 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const unitCategory = {
-	inches: 'length', centimeters: 'length', miles: 'length', kilometers: 'length',
+	inches: 'length', centimeters: 'length', miles: 'length', kilometers: 'length', feet: 'length', yards: 'length', meters: 'length',
 	pounds: 'mass', kilograms: 'mass', ounces: 'mass', grams: 'mass',
 	floz: 'volume', ml: 'volume', gallon: 'volume', liter: 'volume',
 	fahrenheit: 'temperature', celsius: 'temperature'
 };
 
 const baseFactors = {
-	length: { inches: 0.0254, centimeters: 0.01, miles: 1609.34, kilometers: 1000 },
+	length: { inches: 0.0254, centimeters: 0.01, miles: 1609.34, kilometers: 1000, feet: 0.3048, yards: 0.9144, meters: 1 },
 	mass: { pounds: 0.45359237, kilograms: 1, ounces: 0.028349523125, grams: 0.001 },
 	volume: { floz: 0.0295735, ml: 0.001, gallon: 3.78541, liter: 1 }
 };
 
 const unitLabels = {
-	inches: 'in', centimeters: 'cm', miles: 'mi', kilometers: 'km',
+	inches: 'in', feet: 'ft', yards: 'yd', centimeters: 'cm', meters: 'm', miles: 'mi', kilometers: 'km',
 	pounds: 'lb', kilograms: 'kg', ounces: 'oz', grams: 'g',
 	floz: 'fl oz', ml: 'ml', gallon: 'gal', liter: 'L',
 	fahrenheit: '°F', celsius: '°C'
